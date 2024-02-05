47017503c6315d9b801f9f7334ee28be34d51457
Shubham Lal <im.shubhamlal@gmail.com>
2024-02-05 22:41:23 +0530

    add Sales Person solution

---
+++ 0607-Sales Person.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT name
+FROM SalesPerson
+WHERE NOT EXISTS (
+  SELECT 1
+  FROM Orders
+  JOIN Company ON Orders.com_id = Company.com_id
+  WHERE Orders.sales_id = SalesPerson.sales_id AND Company.name = 'RED'
+);
