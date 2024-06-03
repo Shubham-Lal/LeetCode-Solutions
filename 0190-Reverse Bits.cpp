1a7bb999846a033400d806e4951b9805a7044783
Shubham-Lal <im.shubhamlal@gmail.com>
2024-06-03 21:51:12 +0530

    add Reverse Bits solution

---
+++ 0190-Reverse Bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        long long result = 0;
+        for (int i = 0; i<32; i++) {
+            long long leastSignificantBit = n & 1;
+            n = n >> 1;
+            result = result << 1;
+            result = result | leastSignificantBit;
+        }
+        return result;
+    }
+};
